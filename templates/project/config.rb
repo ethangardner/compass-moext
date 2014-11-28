4b456aaeeb53a50f4c447ad5b035d86d1ae137d9
Ethan Gardner <ethan@ethanandjamie.com>
2014-11-28 12:34:26 -0500

    adding styles partial to the project and updating manifest

--- templates/project/config.rb
+++ templates/project/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/styles"
-sass_dir = "assets/sass"
-images_dir = "assets/images"
-javascripts_dir = "assets/scripts"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -3,5 +3,6 @@ description "Momentum Compass Extension - a microlibrary for responsibly scoped 
 # Make sure you list all the project template files here in the manifest.
 stylesheet "_base.scss", :media => "all"
 stylesheet "_reset.scss", :media => "all"
+stylesheet "_styles.scss", :media => "all"
 stylesheet "all.scss", :media => "all"
 javascript "app.js"
