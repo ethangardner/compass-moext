4819aaebf49d97038cc386b58d6e114a36c73b82
Ethan Gardner <ethan@ethanandjamie.com>
2014-11-28 14:42:19 -0500

    reordering properties in all.scss file. cleaning up $colors

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,4 +1,4 @@
-description "Momentum Compass Extension - a microlibrary for responsibly scoped applications and modules"
+description "Momentum Compass Extension - a microlibrary for responsibly scoped modules"
 
 # Make sure you list all the project template files here in the manifest.
 stylesheet "_base.scss", :media => "all"
@@ -6,3 +6,8 @@ stylesheet "_reset.scss", :media => "all"
 stylesheet "_styles.scss", :media => "all"
 stylesheet "all.scss", :media => "all"
 javascript "app.js"
+
+welcome_message %Q{
+Update the application namespace variable, $app-ns in _base.scss
+to get started.
+}
